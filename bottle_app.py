d227108114434cb5c949a072ac66eb9cbf421142
mtj0712 <mtj0712@gmail.com>
2023-03-22 22:45:20 -0400

    Test files: practiced bottle

--- bottle_app.py
+++ bottle_app.py
@@ -1,7 +1,24 @@
-from bottle import default_app, route
+from bottle import default_app, route, run, template, request, get, static_file
 
 @route('/')
-def hello_world():
-    return 'Hello from Bottle!'
+def index():
+    return template('templates/index', name=request.query.name)
 
-application = default_app()
+@route('/css/<filename>')
+def static_css(filename):
+    return static_file(filename, root='css/')
+
+@route('/images/<filename>'):
+def static_image(filename):
+    return static_file(filename, root='images/')
+
+@route('/posted', method='POST')
+def posted():
+    return '<h1>You posted!</h1>'
+
+@get('/weather')
+def getWeather():
+    return {'Today' : 'None'}
+
+# application = default_app()
+run()
